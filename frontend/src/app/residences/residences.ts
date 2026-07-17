e29b9bdeaf8217c860891b94b06cea3b2eb81c4b
Ilyass Cherradi <ilyasscherradi15@gmail.com>
2026-07-17 02:31:08 +0100

    Initial commit: Docker setup + migrations Laravel

---
+++ frontend/src/app/residences/residences.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-residences',
+  imports: [],
+  templateUrl: './residences.html',
+  styleUrl: './residences.css',
+})
+export class Residences {}
