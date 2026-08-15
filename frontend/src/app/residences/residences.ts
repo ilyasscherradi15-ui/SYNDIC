b0d4f7f32132fa42ca87cfc2838d639bf8a796fd
Ilyass Cherradi <ilyasscherradi15@gmail.com>
2026-08-15 15:20:10 +0100

    test: nettoyage fichiers de test obsolètes

--- frontend/src/app/residences/residences.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-residences',
-  imports: [],
-  templateUrl: './residences.html',
-  styleUrl: './residences.css',
-})
-export class Residences {}
